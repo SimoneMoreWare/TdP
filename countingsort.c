fa0bbd43104878d38d5c84ca04a954900409d036
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-08-25 10:26:06 +0200

    Update countingsort.c

--- countingsort.c
+++ countingsort.c
@@ -1,19 +1,20 @@
-void countingsort(int *a,int n){
+void countingsort(int *array,int arraySize){
     /*
      * range 1 7
      * vettore di appoggio b con 7 elementi
      * 1) contare occorenze vettore a e inseriamo nel vettore b
      *
      */
-    int b[8];
-    int i;
+    int i,b[100]; //100 dimensione dell intervallo che va da 1 a 100
+    int res[100];
 
     //inizializzare vettore
-    for(i=0;i<8;i++)
+     for(i=0;i<100;i++){
         b[i]=0;
+    }
     //contare occorenze di ogni valore all'interno del vettore a
-    for(i=0;i<n;i++) {
-        b[a[i]]++;
+    for(i=0;i<arraySize;i++){
+        b[array[i]]++;
     }
     //ulteriore trasformazione
     //in ogni posizione inseriamo
@@ -24,7 +25,7 @@ void countingsort(int *a,int n){
     //quanti elementi <= indice
     //esistono all'interno del vettore a
     //
-    for(i=1;i<8;i++){
+    for(i=1;i<100;i++){
         b[i]=b[i]+b[i-1];
     }
     //Nel vettore risultante l'elemento i sarà messo nella posizione [b[i]]
@@ -32,10 +33,13 @@ void countingsort(int *a,int n){
     //dall'elemento a vado a leggere la posizione del vettore b
     //ci dice che l'elemento a deve essere posizionato nella posizione b[i]
     //decrementare b[i]
-    int ris[10];
-    for(i=9;i>=0;i--){
-        ris[b[a[i]]]=a[i];
-        b[a[i]]--;
+    for(i=arraySize-1;i>=0;i--){
+        res[b[array[i]]-1]=array[i];
+        b[array[i]]--;
     }
-    visualizza(ris,10);
+    
+    for(i=0;i<arraySize;i++){
+        array[i]=res[i];
+    }
+    
 }
